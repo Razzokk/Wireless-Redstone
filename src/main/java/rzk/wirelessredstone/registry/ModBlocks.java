65bcb049578b6bcc17334c6fea465e16c2d8b4aa
Razzokk <razzokk@proton.me>
2020-11-18 23:52:33 +0100

    REMOVED: IItemProvider interface, using a Function now ADDED: defaultItemProperties method to get default item props for item creation

--- src/main/java/rzk/wirelessredstone/registry/ModBlocks.java
+++ src/main/java/rzk/wirelessredstone/registry/ModBlocks.java
@@ -6,10 +6,10 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import rzk.lib.mc.item.IItemProvider;
 import rzk.wirelessredstone.WirelessRedstone;
 import rzk.wirelessredstone.block.BlockFrequency;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ModBlocks
@@ -20,16 +20,16 @@ public class ModBlocks
 	public static final RegistryObject<Block> TRANSMITTER = registerBlock("wireless_transmitter", () -> new BlockFrequency(true));
 	public static final RegistryObject<Block> RECEIVER = registerBlock("wireless_receiver", () -> new BlockFrequency(false));
 
-	public static RegistryObject<Block> registerBlock(String name, Supplier<Block> blockSupplier, IItemProvider itemProvider)
+	public static RegistryObject<Block> registerBlock(String name, Supplier<Block> blockSupplier, Function<Block, BlockItem> itemProvider)
 	{
 		RegistryObject<Block> block = BLOCKS.register(name, blockSupplier);
-		ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().group(WirelessRedstone.ITEM_GROUP_WIRELESS_REDSTONE)));
+		ITEMS.register(name, () -> itemProvider.apply(block.get()));
 		return block;
 	}
 
 	public static RegistryObject<Block> registerBlock(String name, Supplier<Block> blockSupplier)
 	{
-		return registerBlock(name, blockSupplier, block -> new BlockItem(block, new Item.Properties().group(WirelessRedstone.ITEM_GROUP_WIRELESS_REDSTONE)));
+		return registerBlock(name, blockSupplier, block -> new BlockItem(block, ModItems.defaultItemProperties()));
 	}
 
 	public static RegistryObject<Block> registerBlockNoItem(String name, Supplier<Block> blockSupplier)
--- src/main/java/rzk/wirelessredstone/registry/ModItems.java
+++ src/main/java/rzk/wirelessredstone/registry/ModItems.java
@@ -15,4 +15,9 @@ public class ModItems
 	public static final RegistryObject<Item> CIRCUIT = ITEMS.register("wireless_circuit", () -> new Item(new Item.Properties().group(WirelessRedstone.ITEM_GROUP_WIRELESS_REDSTONE)));
 	public static final RegistryObject<Item> REMOTE = ITEMS.register("wireless_remote", ItemWirelessRemote::new);
 	public static final RegistryObject<Item> FREQUENCY_COPIER = ITEMS.register("frequency_copier", ItemFrequencyCopier::new);
+
+	public static Item.Properties defaultItemProperties()
+	{
+		return new Item.Properties().group(WirelessRedstone.ITEM_GROUP_WIRELESS_REDSTONE);
+	}
 }
