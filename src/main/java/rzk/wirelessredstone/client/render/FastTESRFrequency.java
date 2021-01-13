c4f83bc5939488df356ea58a85d0dc263f528271
Razzokk <razzokk@proton.me>
2021-01-13 22:30:14 +0100

    ADDED: Fast tesr for frequency blocks and models

---
+++ src/main/java/rzk/wirelessredstone/client/render/FastTESRFrequency.java
@@ -0,0 +1,33 @@
+package rzk.wirelessredstone.client.render;
+
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraftforge.client.model.animation.FastTESR;
+import org.lwjgl.util.vector.Quaternion;
+import rzk.wirelessredstone.tile.TileFrequency;
+
+public class FastTESRFrequency extends FastTESR<TileFrequency>
+{
+	@Override
+	public void renderTileEntityFast(TileFrequency tile, double x, double y, double z, float partialTicks, int destroyStage, float partial, BufferBuilder buffer)
+	{
+		GlStateManager.pushMatrix();
+		GlStateManager.translate(x + 0.5f, y + 0.5f, z + 0.5f);
+		String s = String.valueOf(65432);
+		FontRenderer fontrenderer = getFontRenderer();
+
+		for (int i = 0; i < 4; i++)
+		{
+			GlStateManager.pushMatrix();
+			GlStateManager.rotate(i * 90, 0f, 1f, 0f);
+			GlStateManager.translate(0f, 0.5f, 0.515625f);
+			GlStateManager.scale(1f / 96, -1f / 96, 1f / 96);
+			fontrenderer.drawString(s, -fontrenderer.getStringWidth(s) / 2, 2, 0);
+			GlStateManager.popMatrix();
+		}
+
+		GlStateManager.popMatrix();
+	}
+}
