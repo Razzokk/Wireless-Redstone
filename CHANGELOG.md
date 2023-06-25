ac73ed008c16e526ec86c20b0557233732609e72
Razzokk <razzokk@proton.me>
2023-06-26 01:42:03 +0200

    ADD: automated publishing and proper readme

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,17 +4,19 @@ Format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 versioning follows [Semantic Versioning](https://semver.org/spec/v2.0.0.html)
 with the minecraft version prepended to it.
 
-## [Unreleased]
+## Unreleased
+
+### Added
+- Proper README.md
+- Automated Modrinth and CurseForge publishing with the [Minotaur](https://github.com/modrinth/minotaur) and [CurseForgeGradle](https://github.com/Darkhax/CurseForgeGradle) gradle tasks/plugins
+- Changelog semi-automation with the [Gradle Changelog Plugin](https://github.com/JetBrains/gradle-changelog-plugin) 
 
 ### Changed
-
 - Update gradle and fabric-loom
 - Separate client more strictly
 
-## [1.20.1-1.0.0] - 2023-06-16
-
-### Port to MC 1.20.1
+## 1.20.1-1.0.0 - 2023-06-16
+Port to MC 1.20.1
 
 ### Changed
-
 - Increased contrast of T and R letters on transmitter and receiver texture
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,48 @@
+import net.darkhax.curseforgegradle.Constants
+import net.darkhax.curseforgegradle.TaskPublishCurseForge
+import org.jetbrains.changelog.Changelog
+
 plugins {
-	id("fabric-loom") version("1.2-SNAPSHOT")
+	java
+	id("fabric-loom") version "1.2-SNAPSHOT"
+	id("com.modrinth.minotaur") version "2.+"
+	id("net.darkhax.curseforgegradle") version "1.1.15"
+	id("org.jetbrains.changelog") version "2.1.0"
 }
 
 val javaVersion: Int = JavaLanguageVersion.of(property("javaVersion").toString()).asInt()
-val minecraftVersion: String by project
+val mcVersion: String by project
 val modId: String by project
 val modVersion: String by project
 val modGroup: String by project
-val modArchivesBaseName: String by project
 val yarnMappings: String by project
 val loaderVersion: String by project
 val fabricApiVersion: String by project
 val clothConfigVersion: String by project
 val modMenuVersion: String by project
+val jeiVersion: String by project
+
+val modReleaseType: String =
+	if (modVersion.lowercase().contains("beta")) "beta"
+	else if (modVersion.lowercase().contains("alpha")) "alpha"
+	else "release"
+
+val curseforgeProjectId: String by project
 
 version = modVersion
 group = modGroup
 
 base {
-	archivesName.set("${modArchivesBaseName}-fabric")
+	archivesName.set("$modId-fabric")
 }
 
 repositories {
+	// Cloth config
 	maven("https://maven.shedaniel.me/")
+	// Mod Menu
 	maven("https://maven.terraformersmc.com/releases/")
+	// JEI
+	maven("https://maven.blamejared.com/")
 }
 
 loom {
@@ -38,7 +57,7 @@ loom {
 }
 
 dependencies {
-	minecraft("com.mojang", "minecraft", minecraftVersion)
+	minecraft("com.mojang", "minecraft", mcVersion)
 	mappings("net.fabricmc", "yarn", yarnMappings, classifier = "v2")
 
 	modImplementation("net.fabricmc", "fabric-loader", loaderVersion)
@@ -46,6 +65,8 @@ dependencies {
 
 	modApi("me.shedaniel.cloth", "cloth-config-fabric", clothConfigVersion)
 	modApi("com.terraformersmc", "modmenu", modMenuVersion)
+
+	modLocalRuntime("mezz.jei", "jei-$mcVersion-fabric", jeiVersion)
 }
 
 tasks {
@@ -58,7 +79,7 @@ tasks {
 				"version" to modVersion,
 				"loader_version" to loaderVersion,
 				"fabric_api_version" to fabricApiVersion,
-				"mcversion" to minecraftVersion,
+				"mcversion" to mcVersion,
 				"java_version" to javaVersion,
 				"cloth_config_version" to clothConfigVersion,
 				"modmenu_version" to modMenuVersion
@@ -75,4 +96,46 @@ java {
 	withSourcesJar()
 	sourceCompatibility = JavaVersion.toVersion(javaVersion)
 	targetCompatibility = JavaVersion.toVersion(javaVersion)
+}
+
+// Publishing
+
+changelog {
+	versionPrefix.set("")
+	groups.empty()
+	combinePreReleases.set(false)
+}
+
+modrinth {
+	token.set(System.getenv("MODRINTH_TOKEN"))
+
+	projectId.set(modId)
+	versionNumber.set("fabric-$modVersion")
+	versionName.set("[Fabric $mcVersion] $modId-$modVersion")
+	versionType.set(modReleaseType)
+	uploadFile.set(tasks.remapJar)
+	changelog.set(provider { project.changelog.renderItem(project.changelog.get(modVersion), Changelog.OutputType.MARKDOWN) })
+
+	dependencies {
+		required.project("fabric-api")
+		optional.project("cloth-config")
+		optional.project("modmenu")
+	}
+
+	if (hasProperty("debug")) debugMode.set(true)
+}
+
+tasks.register<TaskPublishCurseForge>("curseforge") {
+	apiToken = System.getenv("CURSEFORGE_TOKEN")
+
+	val file = upload(curseforgeProjectId, tasks.remapJar)
+	file.displayName = "[Fabric $mcVersion] $modId-$modVersion"
+	file.releaseType = modReleaseType
+	file.changelog = provider { changelog.renderItem(changelog.get(modVersion), Changelog.OutputType.MARKDOWN) }.get()
+	file.changelogType = Constants.CHANGELOG_MARKDOWN
+	file.addJavaVersion("Java $javaVersion")
+	file.addRequirement("fabric-api")
+	file.addOptional("cloth-config", "modmenu")
+
+	if (hasProperty("debug")) debugMode = true
 }
